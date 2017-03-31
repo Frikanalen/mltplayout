42b214e92e9b755839c1172727daca7995d5f3be
Odin Hørthe Omdal <odin.omdal@gmail.com>
2017-03-31 17:38:05 +0100

    Update jukebox in already-run update schedule

--- bin/auto_update_schedule.sh
+++ bin/auto_update_schedule.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+path="$(dirname "$0")"
 while true; do
     ~/frikanalen/env/bin/python ~/frikanalen/fkbeta/pickle_schedule.py
-    python ~/mltplayout/bin/playout_reload_schedule.py localhost 8889
+    $path/update_jukebox.sh
+    python $path/playout_reload_schedule.py localhost 8889
     sleep 3600  # Refresh every hour
 done

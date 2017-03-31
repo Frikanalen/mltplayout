b417c25db28af9fead1c3937f8278488be992050
Odin Hørthe Omdal <odin.omdal@gmail.com>
2017-03-31 15:58:17 +0100

    Add cron script for updating the jukebox file

---
+++ bin/update_jukebox.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Update the jukebox file
+# for running in cron
+set -o errexit
+
+wget -q http://beta.frikanalen.no/api/jukebox_csv -O /tmp/jukebox.csv
+
+if [ "$(cat /tmp/jukebox.csv | wc -l)" -lt 100 ]; then
+  echo Jukebox file is too short
+  exit 1
+fi
+
+cd "$(dirname "$0")"
+mv ../cache/csvdb/jukebox_selection.csv ../cache/csvdb/jukebox_selection.csv.prev
+cp /tmp/jukebox.csv ../cache/csvdb/jukebox_selection.csv

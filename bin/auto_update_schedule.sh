35f60ebdd3b6d3c68757c9c4b068023edceef41d
Odin Hørthe Omdal <odin.omdal@gmail.com>
2017-10-25 17:46:38 +0300

    Don't run two instances of auto_update_schedule at same time

--- bin/auto_update_schedule.sh
+++ bin/auto_update_schedule.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 path="$(dirname "$0")"
+if pidof -o %PPID -x "auto_update_schedule.sh">/dev/null; then
+    echo "Process already running"
+    return;
+fi
 while true; do
     ~/frikanalen/env/bin/python ~/frikanalen/fkbeta/pickle_schedule.py
     $path/update_jukebox.sh

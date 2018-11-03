8cbd5525d641870003daaded7614b453c926b71c
Tore Sinding Bekkedal <toresbe@gmail.com>
2018-11-03 15:27:55 +0100

    Revert "Første utkast til Caspar-avspiller."

--- src/vision/configuration.py
+++ src/vision/configuration.py
@@ -18,7 +18,7 @@ class Configuration(object):
     jobs_root = os.path.join(cache_root, 'jobs')
     ident_media_root = os.path.join(base_path, 'video')
 
-    player_class = 'vision.players.casparcg_player:CasparCGPlayer'
+    player_class = 'vision.players.mlt_player:MLTPlayer'
 
     def config_strings(self):
         l = []
@@ -40,6 +40,9 @@ class FKConfiguration(Configuration):
     render_type = 'broadcast'
     jukebox = True
     video_cache_only = False
+
+    player_class = 'vision.players.mlt_player:MLTDecklinkPlayer'
+
 
 class DeveloperConfiguration(Configuration):
     media_root = './repo/testmedia/media'

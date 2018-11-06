a5cd554981a49523ce0a88f905153fdea1d87e76
Tore Sinding Bekkedal <toresbe@gmail.com>
2018-11-06 17:14:55 +0100

    more robust handling of return data

--- src/vision/players/casparcg.py
+++ src/vision/players/casparcg.py
@@ -18,15 +18,55 @@ class CasparCG:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((self.hostname, self.amcp_port))
 
+    def _read_reply(self):
+        response = self.socket.recv(3)
+
+        try:
+            return_code = int(response)
+        except ValueError:
+            raise ValueError('Did not receive numeric return code from CasparCG')
+
+        while response[-2:] != '\r\n':
+            response += self.socket.recv(1)
+
+        logging.debug('CasparCG replied %s' % (response,))
+
+        response = ''
+        
+        # From the AMCP spec:
+        #
+        # 200 [command] OK - The command has been executed and several lines of
+        # data (seperated by \r\n) are being returned (terminated with an
+        # additional \r\n) 
+        #
+        # 201 [command] OK - The command has been executed and
+        # data (terminated by \r\n) is being returned.
+        # 
+        # 202 [command] OK - The command has been executed.        
+        
+        returned_data = ''
+
+        if return_code == 200: # multiline returned_data
+            while returned_data[-4:] != '\r\n\r\n':
+                returned_data += self.socket.recv(512)
+
+            returned_data = response.splitlines()
+
+        if return_code == 201: # single-line returned_data
+            while returned_data[-2:] != '\r\n':
+                returned_data += self.socket.recv(512)
+
+        if return_code == 202:
+            returned_data = None
+
+        return (return_code, returned_data)
+
     def _send_command(self, command, xmlreply=False):
         self.socket.send("%s\r\n" % command)
         logging.debug("sending command %s" % (command,))
-        bufsize = 4096
-        response = self.socket.recv(bufsize)
-        # FIXME add code to split response in first line and the rest
-        return (response, None)
+        return self._read_reply()[1]
 
 if __name__ == '__main__':
     c = CasparCG()
     c.connect('localhost')
-    print(c._send_command('CLS'))
+    print(c._send_command('INFO 1-50'))

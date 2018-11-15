bb41f12ca65ec572fa389943da22e86a8c48e526
Tore Sinding Bekkedal <toresbe@gmail.com>
2018-11-15 21:05:19 +0100

    pep8ify and fix mixed indent

--- src/test/test_clock.py
+++ src/test/test_clock.py
@@ -5,26 +5,30 @@ from vision import clock
 import time
 import datetime
 
-test_time = datetime.datetime(year=2011, month=7, day=19, hour=12, minute=10, second=0)
+test_time = datetime.datetime(year=2011, month=7, day=19, hour=12,
+                              minute=10, second=0)
+
 
 class ClockTestCase(unittest.TestCase):
     """
-    Tests for L{twistedschedule.task.ScheduledCall} based on a fake L{IReactorTime}
-    implementation.
+    Tests for L{twistedschedule.task.ScheduledCall} based on a fake
+    L{IReactorTime} implementation.
     """
     def testRealtime(self):
-    	clock.set(clock.RealTime())
+        clock.set(clock.RealTime())
         self.assertNotEquals(clock.now(), test_time)
 
         a = clock.now()
         time.sleep(0.1)
         b = clock.now()
         d = b-a
-        self.assertTrue(d.microseconds < 200000, "Not equal, delta should be 100000-200000: delta = %f" % (d.microseconds))
+        self.assertTrue(d.microseconds < 200000,
+                        "Not equal, delta should be 100000-200000: delta = %f"
+                        % (d.microseconds))
 
     def testSimulatedTime(self):
-    	source = clock.SimulatedTime(test_time, ratio=0.0)
-    	clock.set(source)
+        source = clock.SimulatedTime(test_time, ratio=0.0)
+        clock.set(source)
         self.assertEquals(clock.now(), test_time)
         a = clock.now()
         time.sleep(0.1)
@@ -36,11 +40,11 @@ class ClockTestCase(unittest.TestCase):
 
         source.set_ratio(100.0)
         time.sleep(0.125)
-        self.assertAlmostEqual(clock.now().second+clock.now().microsecond/1e6, 17.5, places=0)
+        self.assertAlmostEqual(clock.now().second + clock.now().microsecond / 1e6, 17.5, places=0)
 
     def testManualTime(self):
-    	source = clock.ManualTime(test_time)
-    	clock.set(source)
+        source = clock.ManualTime(test_time)
+        clock.set(source)
         self.assertEquals(clock.now(), test_time)
         a = clock.now()
         time.sleep(0.1)
@@ -51,8 +55,8 @@ class ClockTestCase(unittest.TestCase):
         self.assertEquals(clock.now().second, 5)
 
     def testReset(self):
-    	source = clock.SimulatedTime(test_time, ratio=0.0)
-    	clock.set(source)
+        source = clock.SimulatedTime(test_time, ratio=0.0)
+        clock.set(source)
         self.assertEquals(clock.now(), test_time)
         clock.reset()
         self.assertNotEquals(clock.now(), test_time)
@@ -62,7 +66,3 @@ class ClockTestCase(unittest.TestCase):
         b = clock.now()
         d = b-a
         self.assertTrue(d.microseconds < 200000, "Not equal, delta should be 100000-200000: delta = %f" % (d.microseconds))
-
-
-
-

e34fec89afbfd23fd60780d917944733e0fc7e3c
Petter Reinholdtsen <pere@hungry.com>
2019-01-22 03:23:20 +0100

    Use python3 from auto_update_schedule.sh.

--- bin/auto_update_schedule.sh
+++ bin/auto_update_schedule.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 path="$(dirname "$0")"
-python $path/fetch_and_pickle_schedule.py 10 $path/../cache/dailyplan/
+python3 $path/fetch_and_pickle_schedule.py 10 $path/../cache/dailyplan/
 $path/update_jukebox.sh
-python $path/playout_reload_schedule.py localhost 8889
+python3 $path/playout_reload_schedule.py localhost 8889
